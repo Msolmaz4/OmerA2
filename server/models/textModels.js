7843a7b646dd18a40d089985ce5a00d43cbf129c
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-05-03 12:45:05 +0200

    text model

--- server/models/textModels.js
+++ server/models/textModels.js
@@ -1,2 +1,24 @@
 const  mongoose = require('mongoose')
 
+
+const textSchema = new mongoose.Schema({
+
+     title:{
+        type:String,
+        required:true
+     },
+     description:{
+        type:String,
+
+     },
+     img:{
+        type:String,
+        default:''
+     }
+
+
+
+})
+
+const Text = mongoose.Schema('Text',textSchema) 
+module.exports= Text
