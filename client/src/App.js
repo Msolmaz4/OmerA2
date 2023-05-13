37b4c42ec463479bc4098e98c1372e8ddcd802f4
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-05-13 14:57:22 +0200

    redux-bas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
- <h1 class="text-3xl font-bold underline">
+    <div >
+ <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
     </div>
