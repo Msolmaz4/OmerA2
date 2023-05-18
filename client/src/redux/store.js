38e329a43ac14241211705891a8173ecb794dc04
kyk <86296198+Msolmaz4@users.noreply.github.com>
2023-05-18 14:19:19 +0200

    modalreducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,13 +5,15 @@ import { composeWithDevTools} from  'redux-devtools-extension'
 import userReducer from './reducers/user'
 
 import thunk from 'redux-thunk'
+import modalReducer from './reducers/modal'
 
 const initialState = {
 
 }
 
 const reducers = combineReducers({
-    user:userReducer
+    user:userReducer,
+    modal:modalReducer
 
 })
 
